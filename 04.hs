6062c0cea64668b6976e55f9abf6ffdcd7940660
DenisFrezzato <frezzato.denis@gmail.com>
2019-04-07 18:51:33 +0200

    4

---
+++ 04.hs
@@ -0,0 +1,10 @@
+-- Find the number of elements of a list.
+myLength :: [a] -> Int
+-- myLength = length
+myLength [] = 0
+myLength [_] = 1
+myLength (_:xs) = 1 + myLength xs
+
+myLength' :: [a] -> Int
+myLength' = foldr (\_ -> (+1)) 0
+
