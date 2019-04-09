645eaed4c6374cf250ddbca865919a2f6e4b2780
DenisFrezzato <frezzato.denis@gmail.com>
2019-04-09 18:55:30 +0200

    8

---
+++ 08.hs
@@ -0,0 +1,10 @@
+-- Eliminate consecutive duplicates of list elements.
+compress :: (Eq a) => [a] -> [a]
+compress [] = []
+compress [x] = [x]
+compress (x:x':xs) = if x == x'
+    then compress (x:xs)
+    else [x] ++ compress (x':xs)
+
+compress' :: (Eq a) => [a] -> [a]
+compress' = map head . group
