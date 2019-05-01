1739bf92aadcb98c303557c9a0cc544baf1a3e36
DenisFrezzato <frezzato.denis@gmail.com>
2019-05-01 17:10:26 +0200

    17

---
+++ 17.hs
@@ -0,0 +1,9 @@
+-- Split a list into two parts; the length of the first part is given.
+-- Do not use any predefined predicates. 
+split :: [a] -> Integer -> ([a], [a])
+split xs l = inner xs l []
+    where
+        inner [] _ acc = (acc, [])
+        inner (xs'@(x:xs)) l init
+            | l == 0 = (init, xs')
+            | otherwise = inner xs (l - 1) (init ++ [x])
