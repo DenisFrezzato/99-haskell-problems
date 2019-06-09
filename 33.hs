2b0c3b26be34a7b26fdeecf5025529893714cd88
DenisFrezzato <frezzato.denis@gmail.com>
2019-06-09 19:12:07 +0200

    33

---
+++ 33.hs
@@ -0,0 +1,4 @@
+-- Determine whether two positive integer numbers are coprime.
+-- Two numbers are coprime if their greatest common divisor equals 1.
+coprime :: Int -> Int -> Bool
+coprime x y = gcd x y == 1
