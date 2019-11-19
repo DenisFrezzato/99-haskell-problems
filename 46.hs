b8f8cf59f02fa46b828894595c3f9c0bfad941ae
DenisFrezzato <frezzato.denis@gmail.com>
2019-11-19 13:11:34 +0100

    47

--- 46.hs
+++ 46.hs
@@ -6,23 +6,37 @@
 and' :: Bool -> Bool -> Bool
 and' = (&&)
 
+infix 3 `and'`
+
 or' :: Bool -> Bool -> Bool
 or' = (||)
+
+infix 2 `or'`
 
 nand' :: Bool -> Bool -> Bool
 nand' a b = not $ a && b
 
+infix 3 `nand'`
+
 nor' :: Bool -> Bool -> Bool
 nor' a b = not $ a || b
+
+infix 2 `nor'`
 
 xor' :: Bool -> Bool -> Bool
 xor' a b = a /= b
 
+infix 2 `xor'`
+
 impl' :: Bool -> Bool -> Bool
 impl' a b = not a || b
 
+infix 4 `impl'`
+
 equ' :: Bool -> Bool -> Bool
 equ' = (==)
+
+infix 4 `equ'`
 
 table :: (Bool -> Bool -> Bool) -> [String]
 table f = [show a <> " " <> show b <> " " <> show (f a b) | a <- [True, False], b <- [True, False]]
